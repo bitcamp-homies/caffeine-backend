f818e45ced8395d02661716cab95965f127d0c44
yeonsubak <yeonsubak@gmail.com>
2022-08-28 20:02:06 +0900

    #44 WebMvcConfigurer 사용으로 CorsMapping 주입 완료

--- src/main/java/cafe/controller/CafeController.java
+++ src/main/java/cafe/controller/CafeController.java
@@ -9,7 +9,6 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -188,10 +187,8 @@ public class CafeController {
       BufferedReader bufferedReader = new BufferedReader(reader);
       return bufferedReader.readLine();
     } catch (MalformedURLException e) {
-      // TODO Auto-generated catch block
       e.printStackTrace();
     } catch (IOException e) {
-      // TODO Auto-generated catch block
       e.printStackTrace();
     }
 
