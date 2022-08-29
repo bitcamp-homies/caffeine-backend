1c08983e49b0267f78a751cbc44b48d3ba44dbb6
yeonsubak <yeonsubak@gmail.com>
2022-08-29 15:47:32 +0900

    hotfix: CORS Policy 수정

--- src/main/java/com/bitcamphomies/caffeine/CaffeineApplication.java
+++ src/main/java/com/bitcamphomies/caffeine/CaffeineApplication.java
@@ -9,23 +9,23 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication(scanBasePackages = {"cafe.*"})
-@EntityScan(basePackages = {"cafe.bean"})
+@SpringBootApplication(scanBasePackages = { "cafe.*" })
+@EntityScan(basePackages = { "cafe.bean" })
 @EnableJpaRepositories("cafe.repository")
 @Configuration
 @MapperScan("cafe.repository.mybatis")
 public class CaffeineApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CaffeineApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(CaffeineApplication.class, args);
+  }
 
-  //CORS 이슈 해결
+  // CORS 이슈 해결
   public WebMvcConfigurer corsConfigurer() {
     return new WebMvcConfigurer() {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("https://thumbs.staysane.me");
+        registry.addMapping("/**").allowedOrigins("*");
       }
     };
   }
